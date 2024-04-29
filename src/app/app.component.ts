845472a25faf437449c30ec5dda64e439c9de537
Anastasia Demina <fioB7@yandex.ru>
2024-04-29 17:15:15 +0500

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from '@taiga-ui/core';
+import {
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TUI_SANITIZER,
+    TuiSvgModule,
+    TuiDropdownModule,
+    TuiButtonModule
+} from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SiteSettingsModule } from './site-settings/site-settings.module';
+import { TuiActiveZoneModule, TuiObscuredModule } from "@taiga-ui/cdk";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+    styleUrl: './app.component.scss',
     standalone: true,
     imports: [
         RouterOutlet,
         TuiRootModule,
         TuiDialogModule,
         TuiAlertModule,
-        SiteSettingsModule
+        SiteSettingsModule,
+        TuiSvgModule,
+        TuiObscuredModule,
+        TuiActiveZoneModule,
+        TuiDropdownModule,
+        TuiButtonModule
     ],
     providers: [
         {
@@ -23,4 +37,19 @@ import { SiteSettingsModule } from './site-settings/site-settings.module';
     ]
 })
 export class AppComponent {
+    public open = false;
+
+    public onClick(): void {
+        this.open = !this.open;
+    }
+
+    public onObscured(obscured: boolean): void {
+        if (obscured) {
+            this.open = false;
+        }
+    }
+
+    public onActiveZone(active: boolean): void {
+        this.open = active && this.open;
+    }
 }

d804756471ce1f07746135564d33a9e4aee3e86b
SemyonDuB <dubskikhsemyon@gmail.com>
2024-05-28 23:22:46 +0500

    add builder service

---
+++ src/app/services/builder.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class BuilderService {
+    constructor(private http: HttpClient) {
+    }
+
+    private Build(organisation: string,
+                  config: object,
+                  ...files: File[]): void {
+
+        let config_blob = new Blob([JSON.stringify(config, null, 2)], {
+            type: "application/json",
+        });
+
+        const formData = new FormData();
+        formData.append("organisation", organisation);
+        formData.append("config", config_blob, "config.json");
+
+        for (const file of files) {
+            formData.append(file.name, file, file.name);
+        }
+
+        const requestOptions = {
+            method: "POST",
+            body: formData,
+            redirect: "follow"
+        };
+
+        this.http.post('http://178.154.224.222:5282/build', formData);
+    }
+}
